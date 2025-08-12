57227557274ab9bce09c7ddeca06cf0ed9c2460b
Quang D. <quang.dangngoc@teragroup.io>
2025-08-12 18:30:14 +0700

    fix: read env

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    READ_CAPTCHA_WITH_AI: process.env.READ_CAPTCHA_WITH_AI,
+    DISPLAY_COPY_CURL_BUTTON: process.env.DISPLAY_COPY_CURL_BUTTON,
+    DISPLAY_AUTO_REGISTER_BUTTON: process.env.DISPLAY_AUTO_REGISTER_BUTTON,
+    GOOGLE_GENAI_API_KEY: process.env.GOOGLE_GENAI_API_KEY,
+  },
+};
 
 export default nextConfig;
